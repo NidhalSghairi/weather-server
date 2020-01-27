4e22d40fcfce674715d398613131cf41e449d713
NidhalSghairi <nidhal.sghairi@ensi-uma.tn>
2020-01-27 01:19:14 +0100

    deployement

--- public/js/app.js
+++ public/js/app.js
@@ -1,6 +1,6 @@
 const search =()=>{
     const adresse= document.getElementById('test').value
-    const url = "http://localhost:3000/weather?adresse="+adresse
+    const url = "/weather?adresse="+adresse
     fetch(url).then((response)=>{
         response.json().then((data)=>{
             document.getElementById('location').innerHTML = data.location
